1e08d37edbdbbfd9c7767f1885b483237d00e692
khaeru99 <khaerul.r.23@gmail.com>
2025-12-12 22:04:11 +0700

    update: token auto logout

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (userId, role) => {
   return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
-    expiresIn: '7d', // Token berlaku 7 hari
+    expiresIn: '2h', // Token berlaku 7 hari
   });
 };
 
