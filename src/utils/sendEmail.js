3c6fa80300b0fefd83d09e9dbbcc44c02e3ed56b
khaeru99 <khaerul.r.23@gmail.com>
2025-12-12 20:46:47 +0700

    fix bug: forgot password menggunakan email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,17 +2,12 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   // Buat transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST, // smtp.zoho.com
-    port: Number(process.env.EMAIL_PORT), // Pastikan jadi angka
-    secure: process.env.EMAIL_PORT == 465, // True jika 465, False jika 587
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // Otomatis mengatur Host=smtp.gmail.com dan Port=465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    },
-    // Tambahan logger untuk cek error di Railway
-    logger: true,
-    debug: true
+    }
   });
 
   const mailOptions = {
